7ab4b7f3048b02d7cb7dc5542f22f5c7f65e3452
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-13 15:21:18 +0100

    sout

--- src/quizkampen/Client.java
+++ src/quizkampen/Client.java
@@ -15,6 +15,8 @@ public class Client {
             ObjectOutputStream out = new ObjectOutputStream(bridge.getOutputStream());
             ObjectInputStream in = new ObjectInputStream(bridge.getInputStream());
             session = (SessionQ)in.readObject();
+            System.out.println(session.getQuestion().question);
+            out.writeObject(session);
         } catch (IOException | ClassNotFoundException ex) {
             System.out.println(ex.getMessage());
         }
