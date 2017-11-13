83de6651dc55d7092f467a59e373427a081fd9b5
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-13 13:35:42 +0100

    Client skiss

---
+++ src/Client.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client {
+    
+    Socket bridge;
+
+    public Client() {
+        try {
+            bridge = new Socket("127.0.0.1", 33333);
+            ObjectOutputStream out = new ObjectOutputStream()
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+        Client c = new Client();
+    }
+
+}
--- src/quizkampen/Quizkampen.java
+++ src/quizkampen/Quizkampen.java
@@ -14,5 +14,4 @@ public class Quizkampen {
 
         System.out.println("Claudia");
     }
-
 }
