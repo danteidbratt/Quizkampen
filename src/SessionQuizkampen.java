7ca4e76b4f4c0e1aa0962d7e1163a03f820e1cdf
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2017-11-13 13:47:51 +0100

    hej

---
+++ src/SessionQuizkampen.java
@@ -0,0 +1,40 @@
+
+public class SessionQuizkampen {
+    //portnummer = 33333
+   protected String username; 
+   protected String question; 
+   protected String alternative; 
+   protected String answer; 
+   
+  
+    protected String getUsername(){
+        return username;
+    }
+    
+    protected String getQuestion(){
+        return question;
+    }
+    protected String getAlternative(){
+        return alternative;  
+    }
+    
+    protected String getAnswer() {
+        return answer; 
+    }
+    
+    protected void setUsername(String u){
+        username = u;
+    }
+    
+    protected void setQuestion(String q){
+        question = q;
+    }
+    
+    protected void setAlternative(String a){
+        alternative = a;
+    }
+    
+    protected void setAnswer(String s){
+        answer = s;
+    }
+}
