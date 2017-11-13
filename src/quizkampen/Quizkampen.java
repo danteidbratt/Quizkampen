5ff44ac1648043776e8aae4b877aacb8ee1ae414
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2017-11-13 10:20:30 +0100

    hej

--- src/quizkampen/Quizkampen.java
+++ src/quizkampen/Quizkampen.java
@@ -7,6 +7,7 @@ public class Quizkampen {
 		System.out.println("Hej det här är min test //David");
 		System.out.println("Det här kommer bli rolig! //David");
         System.out.println("Ja! // Anna");
+        System.out.println("Claudia");
     }
 
 }
