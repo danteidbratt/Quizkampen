4da760e647fe3306c14396b83dc8a004e64349cb
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2017-11-13 13:51:16 +0100

    hje

--- src/quizkampen/SessionQ.java
+++ src/quizkampen/SessionQ.java
@@ -1,11 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizkampen;
 
-public class SessionQuizkampen {
-    //portnummer = 33333
+/**
+ *
+ * @author claudiastenberg
+ */
+public class SessionQ {
+     //portnummer = 33333
    protected String username; 
    protected String question; 
    protected String alternative; 
    protected String answer; 
-   
+  
   
     protected String getUsername(){
         return username;
