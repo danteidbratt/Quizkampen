17ed973ee123f74726543a28d412b4dcd11ca67e
David Larsson <david.larsson89@gmail.com>
2017-11-13 14:31:57 +0100

    Server

---
+++ src/quizkampen/Server.java
@@ -0,0 +1,43 @@
+package quizkampen;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Server {
+	private Socket clienSocket1;
+	private Socket clienSocket2;
+	private List<Question> questions = new ArrayList<>();
+	private Question tempQuestion;
+	
+	public Server(Socket clientSocket1, Socket clientSocket2) throws IOException{
+		this.clienSocket1 = clientSocket1;
+		this.clienSocket2 = clientSocket2;
+		readQuestionsFromFile();
+		
+		ObjectOutputStream user1Output = new ObjectOutputStream(clientSocket1.getOutputStream());
+		ObjectInputStream user1Input = new ObjectInputStream(clientSocket1.getInputStream());
+		
+		ObjectOutputStream user2Output = new ObjectOutputStream(clientSocket2.getOutputStream());
+		ObjectInputStream user2Input = new ObjectInputStream(clientSocket2.getInputStream());
+	}
+	public void readQuestionsFromFile() throws FileNotFoundException {
+		File file = new File("Questions_database.txt");
+		Scanner sc = new Scanner(file);
+		while (sc.hasNextLine()) {
+			tempQuestion = new Question(sc.nextLine());
+            tempQuestion.setAnswerAlternatives(sc.nextLine(), true);
+            tempQuestion.setAnswerAlternatives(sc.nextLine(), false);
+            tempQuestion.setAnswerAlternatives(sc.nextLine(), false);
+            tempQuestion.setAnswerAlternatives(sc.nextLine(), false);
+			questions.add(tempQuestion);
+        }
+        sc.close();
+	}
+}
