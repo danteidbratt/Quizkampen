7b6535466189e1401657461341007154f1317161
David Larsson <david.larsson89@gmail.com>
2017-11-14 13:40:43 +0100

    Najs

--- src/quizkampen/Window.java
+++ src/quizkampen/Window.java
@@ -1,13 +1,21 @@
 package quizkampen;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
-public class Window extends JFrame{
-	WelcomeScreen ws = new WelcomeScreen();
+public class Window extends JFrame implements ActionListener{
+	WelcomeScreen ws = new WelcomeScreen(this);
+	
 	public Window() {
 		add(ws);
 		setSize(800, 800);
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 }
