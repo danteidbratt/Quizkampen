f2638ab7dadcacd91df1ff7f938ca2dc49759478
Anna Wrang <anna.wrang@gmail.com>
2017-11-14 13:15:25 +0100

    hej

--- src/quizkampen/LobbyScreen.java
+++ src/quizkampen/LobbyScreen.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- *  och öppna templates :)
- */
 package quizkampen;
+import java.awt.BorderLayout;
+import javax.swing.*;
 
-/**
- *
- * @author Anna
- */
-public class LobbyScreen {
+public class LobbyScreen extends JPanel{
+    protected JLabel opponentIs = new JLabel("Your opponent is...");
+    protected JButton subjectOne= new JButton("Subject 1");
+    protected JButton subjectTwo = new JButton("Subject 2");
+    protected JButton subjectThree = new JButton("Subject 3");
+    protected JButton start = new JButton("Start");
     
+    public LobbyScreen(){
+        this.setLayout(new BorderLayout());
+        this.add(opponentIs, BorderLayout.NORTH);
+        this.add(subjectOne, BorderLayout.WEST);
+        this.add(subjectTwo, BorderLayout.CENTER);
+        this.add(subjectThree, BorderLayout.EAST);
+        this.add(start, BorderLayout.SOUTH);
+    }
 }
