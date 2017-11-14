a340b2b50821816c3ee2a7ea907de9f6461a15b1
Anna Wrang <anna.wrang@gmail.com>
2017-11-14 15:54:06 +0100

    databas fixad - funkar inte än

--- src/quizkampen/Client.java
+++ src/quizkampen/Client.java
@@ -14,14 +14,15 @@ public class Client {
             bridge = new Socket("127.0.0.1", 33333);
             System.out.println("hej");
 //            Gui gui = new Gui();
-			Window w = new Window();
-            ObjectOutputStream out = new ObjectOutputStream(bridge.getOutputStream());
-                        
+            Window w = new Window();
+            ObjectOutputStream out = new ObjectOutputStream(bridge.getOutputStream());                   
             ObjectInputStream in = new ObjectInputStream(bridge.getInputStream());
             
             session = (SessionQ)in.readObject();
-            System.out.println(session.getQuestion().question);
-            out.writeObject(session);
+            System.out.println(session.proposedSubjectOne.get(0).question);
+            
+//            System.out.println(session.getQuestion().question);
+//            out.writeObject(session);
             
         } catch (IOException | ClassNotFoundException ex) {
             System.out.println(ex.getMessage());
--- src/quizkampen/SessionQ.java
+++ src/quizkampen/SessionQ.java
@@ -1,51 +1,95 @@
-
 package quizkampen;
 
 import java.io.Serializable;
+import java.util.List;
 
-public class SessionQ implements Serializable{
-     //portnummer = 33333
-   protected String username; 
-   protected Question question; 
-   protected String alternative; 
-   protected String answer; 
-  
-  SessionQ () {
-      
-      
-      
-      
-  }
-   
-   
-    public String getUsername(){
+public class SessionQ implements Serializable {
+    //portnummer = 33333
+
+    protected String username;
+    protected Question question;
+    protected String alternative;
+    protected String answer;
+    protected String chosenSubjectOne;
+    protected String chosenSubjectTwo;
+    public List<Question> proposedSubjectOne;
+    protected List<Question> proposedSubjectTwo;
+    protected List<Question> proposedSubjectThree;
+
+    SessionQ() {
+
+    }
+
+    public String getUsername() {
         return username;
     }
-    
-    public Question getQuestion(){
+
+    public Question getQuestion() {
         return question;
     }
-    public String getAlternative(){
-        return alternative;  
+
+    public String getAlternative() {
+        return alternative;
     }
-    
+
     public String getAnswer() {
-        return answer; 
+        return answer;
     }
-    
-    public void setUsername(String u){
+
+    public void setUsername(String u) {
         username = u;
     }
-    
-    public void setQuestion(Question q){
+
+    public void setQuestion(Question q) {
         question = q;
     }
-    
-    public void setAlternative(String a){
+
+    public void setAlternative(String a) {
         alternative = a;
     }
-    
-    public void setAnswer(String s){
+
+    public void setAnswer(String s) {
         answer = s;
     }
+
+    public void setChosenSubjectOne(String chosenSubject) {
+        this.chosenSubjectOne = chosenSubject;
+    }
+
+    public void setChosenSubjectTwo(String chosenSubject) {
+        this.chosenSubjectTwo = chosenSubject;
+    }
+
+    public String getChosenSubjectOne() {
+        return chosenSubjectOne;
+    }
+
+    public String getChosenSubjectTwo() {
+        return chosenSubjectTwo;
+    }
+
+    public void setProposedSubjectOne(List lista) {
+        this.proposedSubjectOne = lista;
+    }
+
+    public void setProposedSubjectTwo(List lista) {
+        this.proposedSubjectTwo = lista;
+    }
+
+    public void setProposedSubjectThree(List lista) {
+        this.proposedSubjectThree = lista;
+    }
+
+    public List getProposedSubjectOne() {
+        return proposedSubjectOne;
+    }
+
+    public List getProposedSubjectTwo() {
+        return proposedSubjectTwo;
+    }
+
+    public List getProposedSubjectThree() {
+        return proposedSubjectThree;
+    }
+    
 }
