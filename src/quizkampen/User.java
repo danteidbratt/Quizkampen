21af6a5afa71b545eabeb03f0fa080d50b55feb4
Daven <daven.maenpaa@outlook.com>
2017-11-15 13:49:35 +0100

    Tar bort lite

--- src/quizkampen/User.java
+++ src/quizkampen/User.java
@@ -15,29 +15,4 @@ public class User implements Serializable {
     public User(String userName) {
         this.userName = userName;
     }
-
-    public void newUser(String userName) {
-        if(userExist()) {
-            new User(userName);
-
-        } else {
-
-        }
-    }
-
-    public void writeToFile() {
-
-    }
-
-    public boolean userExist() {
-        return true;
-    }
-
-    public void findUser() {
-
-    }
-
-    public String getUserName() {
-        return userName;
-    }
 }
