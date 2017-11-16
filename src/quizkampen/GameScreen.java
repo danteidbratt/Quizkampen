b48e90a45f8fa5a8d2984a815f1ca8a85e35255b
David Larsson <david.larsson89@gmail.com>
2017-11-16 12:01:48 +0100

    kommmmm

--- src/quizkampen/GameScreen.java
+++ src/quizkampen/GameScreen.java
@@ -3,11 +3,15 @@ package quizkampen;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class GameScreen extends JPanel {
 	private final JPanel centerPanel = new JPanel();
@@ -21,9 +25,13 @@ public class GameScreen extends JPanel {
 	JLabel roundTextLabel = new JLabel("Round");
 	JLabel roundBoxLabel = new JLabel("1/2");
 	JLabel roundSpace = new JLabel("");
-	JPanel roundQuestionsPanel = new JPanel();
-	JLabel questionBox1 = new JLabel("");
-	JLabel questionBox2 = new JLabel("");
+	JPanel questionsPanel = new JPanel();
+	List<JLabel> questionBoxes = new ArrayList<>();
+//	JLabel questionBox1 = new JLabel("");
+//	JLabel questionBox2 = new JLabel("");
+//	JLabel questionBox3 = new JLabel("");
+//	JLabel questionBox4 = new JLabel("");
+//	JLabel questionBox5 = new JLabel("");
 	
     JButton questionButton = new JButton("Vilken huvudstad är störst i Norden?");
     JButton answer1Button = new JButton("1. Oslo");
@@ -33,24 +41,70 @@ public class GameScreen extends JPanel {
 
     Color backgroundColor;
     Font buttonFont;
+	int amountOfQuestions = 2;
 
-    public GameScreen(ActionListener al, Font buttonFont, Color backgroundColor) {
+    public GameScreen(Font buttonFont, Color backgroundColor) {
         this.backgroundColor = backgroundColor;
         this.buttonFont = buttonFont;
     }
 
     public void setPanel() {
         setLayout(new BorderLayout());
+		setBackground(backgroundColor);
 		topSpace.setPreferredSize(new Dimension(0, 50));
 		sideSpaceWest.setPreferredSize(new Dimension(70, 0));
         sideSpaceEast.setPreferredSize(new Dimension(70, 0));
         bottomSpace.setPreferredSize(new Dimension(0, 120));
 		
 		centerPanel.setLayout(new GridLayout(3, 1));
+		centerPanel.add(roundPanel);
 		
 		roundPanel.setLayout(new BoxLayout(roundPanel, BoxLayout.Y_AXIS));
-		roundPanel.
-		
+		roundPanel.setBackground(backgroundColor);
+		roundTextLabel.setFont(buttonFont);
+		roundBoxLabel.setPreferredSize(new Dimension(60, 60));
+		roundBoxLabel.setFont(buttonFont);
+		roundBoxLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		questionsPanel.setLayout(new FlowLayout());
+		questionsPanel.setBackground(backgroundColor);
+		for(int i = 0; i < amountOfQuestions; i++) {
+			questionBoxes.add(new JLabel(""));
+			questionBoxes.get(i).setPreferredSize(new Dimension(60, 37));
+			questionBoxes.get(i).setOpaque(true);
+			questionBoxes.get(i).setBackground(Color.gray);
+			questionBoxes.get(i).setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+			questionPanel.add(questionBoxes.get(i));
+		}
+//		questionBox1.setPreferredSize(new Dimension(60, 37));
+//		questionBox1.setOpaque(true);
+//		questionBox1.setBackground(Color.gray);
+//		questionBox1.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+//		questionBox2.setPreferredSize(new Dimension(60, 37));
+//		questionBox2.setOpaque(true);
+//		questionBox2.setBackground(Color.gray);
+//		questionBox2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		questionBox3.setPreferredSize(new Dimension(60, 37));
+//		questionBox3.setOpaque(true);
+//		questionBox3.setBackground(Color.gray);
+//		questionBox3.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		questionBox4.setPreferredSize(new Dimension(60, 37));
+//		questionBox4.setOpaque(true);
+//		questionBox4.setBackground(Color.gray);
+//		questionBox4.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		questionBox5.setPreferredSize(new Dimension(60, 37));
+//		questionBox5.setOpaque(true);
+//		questionBox5.setBackground(Color.gray);
+////		questionBox5.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		questionsPanel.add(questionBox1);
+//		questionsPanel.add(questionBox2);
+//		questionsPanel.add(questionBox3);
+//		questionsPanel.add(questionBox4);
+//		questionsPanel.add(questionBox5);
+		roundSpace.setPreferredSize(new Dimension(0, 20));
+		roundPanel.add(roundTextLabel);
+		roundPanel.add(roundBoxLabel);
+		roundPanel.add(roundSpace);
+		roundPanel.add(questionsPanel);
 		
 		add(topSpace, BorderLayout.NORTH);
 		add(centerPanel, BorderLayout.CENTER);
