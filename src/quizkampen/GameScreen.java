4c0c6d126b60e0114edc6d6864c0aa19d5723e14
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-16 12:21:24 +0100

    hmm

--- src/quizkampen/GameScreen.java
+++ src/quizkampen/GameScreen.java
@@ -10,20 +10,21 @@ import java.awt.event.ActionListener;
 import java.time.Year;
 
 public class GameScreen extends JPanel implements IPanel {
-	private final JPanel centerPanel = new JPanel();
-	private final JPanel roundPanel = new JPanel();
-	private final JPanel questionPanel = new JPanel();
-	private final JLabel topSpace = new JLabel("");
+
+    private final JPanel centerPanel = new JPanel();
+    private final JPanel roundPanel = new JPanel();
+    private final JPanel questionPanel = new JPanel();
+    private final JLabel topSpace = new JLabel("");
     private final JLabel sideSpaceWest = new JLabel("");
     private final JLabel sideSpaceEast = new JLabel("");
     private final JLabel bottomSpace = new JLabel("");
-	
-	JLabel roundTextLabel = new JLabel("Round");
-	JLabel roundBoxLabel = new JLabel("1/2");
-	JLabel roundSpace = new JLabel("");
-	JPanel roundQuestionsPanel = new JPanel();
-	JLabel questionBox1 = new JLabel("");
-	JLabel questionBox2 = new JLabel("");
+
+    JLabel roundTextLabel = new JLabel("Round");
+    JLabel roundBoxLabel = new JLabel("1/2");
+    JLabel roundSpace = new JLabel("");
+    JPanel roundQuestionsPanel = new JPanel();
+    JLabel questionBox1 = new JLabel("");
+    JLabel questionBox2 = new JLabel("");
     JButton questionButton = new JButton("Vilken huvudstad är störst i Norden?");
     JButton answer1Button = new JButton("1. Oslo");
     JButton answer2Button = new JButton("2. Stockholm");
@@ -41,22 +42,20 @@ public class GameScreen extends JPanel implements IPanel {
     @Override
     public void setPanel() {
         setLayout(new BorderLayout());
-		topSpace.setPreferredSize(new Dimension(0, 50));
-		sideSpaceWest.setPreferredSize(new Dimension(70, 0));
+        topSpace.setPreferredSize(new Dimension(0, 50));
+        sideSpaceWest.setPreferredSize(new Dimension(70, 0));
         sideSpaceEast.setPreferredSize(new Dimension(70, 0));
         bottomSpace.setPreferredSize(new Dimension(0, 120));
-		
-		centerPanel.setLayout(new GridLayout(3, 1));
-		
-		roundPanel.setLayout(new BoxLayout(roundPanel, BoxLayout.Y_AXIS));
-		roundPanel.
-		
-		
-		add(topSpace, BorderLayout.NORTH);
-		add(centerPanel, BorderLayout.CENTER);
+
+        centerPanel.setLayout(new GridLayout(3, 1));
+
+        roundPanel.setLayout(new BoxLayout(roundPanel, BoxLayout.Y_AXIS));
+        roundPanel.
+                add(topSpace, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
         add(sideSpaceWest, BorderLayout.WEST);
         add(sideSpaceEast, BorderLayout.EAST);
-		add(bottomSpace, BorderLayout.SOUTH);
+        add(bottomSpace, BorderLayout.SOUTH);
     }
 
     @Override
