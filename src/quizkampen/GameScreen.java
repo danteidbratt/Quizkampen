95c30e9a880d0b70d0f2329d7bbc8bc4bff76e5b
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-16 13:36:04 +0100

    GameScreen ok

--- src/quizkampen/GameScreen.java
+++ src/quizkampen/GameScreen.java
@@ -2,29 +2,31 @@ package quizkampen;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.*;
 import java.awt.event.ActionListener;
-import java.time.Year;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class GameScreen extends JPanel implements IPanel {
 
     private final JPanel centerPanel = new JPanel();
-    private final JPanel roundPanel = new JPanel();
-    private final JPanel questionsPanel = new JPanel();
+    private JPanel roundPanel = new JPanel();
+    private JPanel questionsPanel = new JPanel();
+    private final JLabel logo = new JLabel("QuizFights");
     private final JLabel topSpace = new JLabel("");
     private final JLabel sideSpaceWest = new JLabel("");
     private final JLabel sideSpaceEast = new JLabel("");
     private final JLabel bottomSpace = new JLabel("");
+    private final JPanel cardsPanel = new JPanel();
+    private final JPanel answerCardsPanel= new JPanel();
 
     JLabel roundTextLabel = new JLabel("Round");
-    JLabel roundBoxLabel = new JLabel("1/2");
+    JLabel roundBoxLabel = new JLabel("  1/2  ");
     JLabel roundSpace = new JLabel("");
     List<JLabel> questionBoxes = new ArrayList<>();
     JButton questionButton = new JButton("Vilken huvudstad är störst i Norden?");
@@ -35,7 +37,7 @@ public class GameScreen extends JPanel implements IPanel {
 
     Color backgroundColor;
     Font buttonFont;
-    int amountOfQuestions = 2;
+    int amountOfQuestions = 5;
 
     public GameScreen(Font buttonFont, Color backgroundColor) {
         this.backgroundColor = backgroundColor;
@@ -49,24 +51,28 @@ public class GameScreen extends JPanel implements IPanel {
         topSpace.setPreferredSize(new Dimension(0, 50));
         sideSpaceWest.setPreferredSize(new Dimension(70, 0));
         sideSpaceEast.setPreferredSize(new Dimension(70, 0));
-        bottomSpace.setPreferredSize(new Dimension(0, 120));
+        bottomSpace.setPreferredSize(new Dimension(0, 80));
 
-        centerPanel.setLayout(new GridLayout(3, 1));
-        centerPanel.add(roundPanel);
+        centerPanel.setLayout(new BorderLayout(0, 50));
+        centerPanel.setBackground(backgroundColor);
 
         roundPanel.setLayout(new BoxLayout(roundPanel, BoxLayout.Y_AXIS));
         roundPanel.setBackground(backgroundColor);
         roundTextLabel.setFont(buttonFont);
-        roundBoxLabel.setPreferredSize(new Dimension(60, 60));
+        roundTextLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        roundBoxLabel.setBackground(Color.WHITE);
+        roundBoxLabel.setOpaque(true);
+        roundBoxLabel.setPreferredSize(new Dimension(100, 37));
         roundBoxLabel.setFont(buttonFont);
         roundBoxLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        roundBoxLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         questionsPanel.setLayout(new FlowLayout());
         questionsPanel.setBackground(backgroundColor);
         for (int i = 0; i < amountOfQuestions; i++) {
             questionBoxes.add(new JLabel(""));
             questionBoxes.get(i).setPreferredSize(new Dimension(60, 37));
             questionBoxes.get(i).setOpaque(true);
-            questionBoxes.get(i).setBackground(Color.gray);
+            questionBoxes.get(i).setBackground(Color.WHITE);
             questionBoxes.get(i).setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
             questionsPanel.add(questionBoxes.get(i));
         }
@@ -76,7 +82,36 @@ public class GameScreen extends JPanel implements IPanel {
         roundPanel.add(roundBoxLabel);
         roundPanel.add(roundSpace);
         roundPanel.add(questionsPanel);
+        
+        cardsPanel.setLayout(new GridLayout(2, 1, 0, 50));
+        cardsPanel.setBackground(backgroundColor);
+        answerCardsPanel.setLayout(new GridLayout(2, 2, 10, 10));
+        answerCardsPanel.setBackground(backgroundColor);
+        answer1Button.setFont(buttonFont);
+        answer2Button.setFont(buttonFont);
+        answer3Button.setFont(buttonFont);
+        answer4Button.setFont(buttonFont);
+        answer1Button.setBackground(Color.GREEN);
+        answer1Button.setOpaque(true);
+        answer1Button.setBorderPainted(false);
+        answerCardsPanel.add(answer1Button);
+        answerCardsPanel.add(answer2Button);
+        answerCardsPanel.add(answer3Button);
+        answerCardsPanel.add(answer4Button);
+        questionButton.setFont(new Font("SansSarif", Font.BOLD, 15));
+        cardsPanel.add(questionButton);
+        cardsPanel.add(answerCardsPanel);
+        
+        logo.setBackground(backgroundColor);
+        logo.setOpaque(true);
+        logo.setHorizontalAlignment(SwingConstants.CENTER);
+        logo.setForeground(Color.YELLOW);
+        logo.setFont(new Font("SansSarif", 3, 60));
 
+        centerPanel.add(roundPanel, BorderLayout.NORTH);
+        centerPanel.add(cardsPanel);
+        centerPanel.add(logo, BorderLayout.SOUTH);
+        
         add(topSpace, BorderLayout.NORTH);
         add(centerPanel, BorderLayout.CENTER);
         add(sideSpaceWest, BorderLayout.WEST);
