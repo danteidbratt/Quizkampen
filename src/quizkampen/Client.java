cdd18c7ee5aa978c32df21fa307eb39e55ee33b9
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-21 10:35:49 +0100

    radbyte mm

--- src/quizkampen/Client.java
+++ src/quizkampen/Client.java
@@ -1,20 +1,12 @@
 package quizkampen;
 
-import java.net.Socket;
-
 public class Client {
 
-    Socket bridge;
-    SessionQ session;
     Window w;
 
     public Client() {
         Window w = new Window();
-
         w.setFrame();
-//        w.ws.setPanel();
-//        w.ws.setActionListener(w);
-
     }
     
     public static void main(String[] args) {
