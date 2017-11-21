1c5e4bc7eb5d914f30feb2699a79c21575e9f344
Anna Wrang <anna.wrang@gmail.com>
2017-11-21 13:23:08 +0100

    lite kommentarer

--- src/quizkampen/Server.java
+++ src/quizkampen/Server.java
@@ -13,32 +13,32 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Server implements Runnable {
+public class Server {
 
     private Socket clientSocket1;
     private Socket clientSocket2;
-//    private List<Question> questions = new ArrayList<>();
-//    private Question tempQuestion;
     private SessionQ session;
     protected Database database = new Database();
-//    protected boolean waitingForClient2 = false;
-    protected Thread updateSubjects;
 
     public Server(Socket clientSocket1) {
         try {
             session = new SessionQ();
             database.loadThreeSubjects(session);
-            
 
             ObjectOutputStream user1Output = new ObjectOutputStream(clientSocket1.getOutputStream());
             ObjectInputStream user1Input = new ObjectInputStream(clientSocket1.getInputStream());
 
             user1Output.writeObject(session);
-            session = (SessionQ)user1Input.readObject();
+//            while ((session = (SessionQ) user1Input.readObject()) != null) {
+//                if (session.getRequestingNewSubjects()) {    //  - metod som fyller på frågor i session objektet
+//                    database.loadThreeSubjects(session);
+//                    session.setRequestingNewSubjects(false);
+//                }
+//                user1Output.writeObject(session);
+//            }
+            session = (SessionQ) user1Input.readObject();
 
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -52,19 +52,21 @@ public class Server implements Runnable {
             user2Input = new ObjectInputStream(clientSocket2.getInputStream());
 
             user2Output.writeObject(session);
+            session = (SessionQ) user2Input.readObject();
+
+//            while ((session = (SessionQ) user2Input.readObject()) != null) {
+//                if (session.getRequestingNewSubjects()) {    //  - metod som fyller på frågor i session objektet
+//                    database.loadThreeSubjects(session);
+//                    session.setRequestingNewSubjects(false);
+//                }
+//                user2Output.writeObject(session);
+//            }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    @Override
-    public void run() {
-        while (true) {
-            if (session.getRequestingNewSubjects()) {    // FIXAR SEN / ANNA&CLAUDIA
-                database.loadThreeSubjects(session);
-                session.setRequestingNewSubjects(false);
-            }
-        }
-    }
-
 }
+
