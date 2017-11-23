f27a834d2df953f3a41d5b693619ff85137834e8
danteidbratt <danteidbratt@users.noreply.github.com>
2017-11-23 13:40:22 +0100

    Tja

--- src/quizkampen/ActionHandler.java
+++ src/quizkampen/ActionHandler.java
@@ -1,5 +1,6 @@
 package quizkampen;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -59,7 +60,7 @@ public class ActionHandler implements ActionListener {
 
                 System.out.println("Du är spelare nr: " + w.getPlayerNumber());
 
-                SessionHandler sessionHandler = new SessionHandler(w.session);
+//                SessionHandler sessionHandler = new SessionHandler(w.session);
 
                 w.outGameServer.writeObject(w.session);
 
@@ -67,6 +68,7 @@ public class ActionHandler implements ActionListener {
                 w.rs.setPanel();
                 w.rs.setActionListener(this);
 
+                w.tempQuestions = new Question[w.session.getTotalQsInRound()];
                 for (int i = 0; i < 3; i++) {
                     w.tempSubjects[i] = w.session.getSubject();
                 }
@@ -85,8 +87,7 @@ public class ActionHandler implements ActionListener {
             w.add(w.gs);
         } else if (e.getSource() == w.gs.nextQuestionButton) {
             if (w.questionCounter < w.session.getTotalQsInRound() - 1) {
-                w.gs.setNextQuestion(w.tempSubjects[w.tempIndex].getQuestion());
-                w.questionCounter++;
+                w.gs.setNextQuestion(w.tempQuestions[w.questionCounter++]);
                 w.gs.setButtonActionListener(this);
 
             } else {
@@ -138,12 +139,15 @@ public class ActionHandler implements ActionListener {
 
         for (int i = 0; i < w.ls.subjectButtons.length; i++) {
             if (e.getSource() == w.ls.subjectButtons[i]) {
+                for (int j = 0; j < w.tempQuestions.length; j++) {
+                    w.tempQuestions[j] = w.tempSubjects[i].getQuestion();
+                }
                 w.ls.subjectButtons[i].setBackground(Color.YELLOW);
                 w.ls.subjectButtons[i].setBorderPainted(false);
                 w.ls.subjectButtons[i].setOpaque(true);
                 w.ls.startButton.setVisible(true);
                 w.tempIndex = i;
-                w.gs.setNextQuestion(w.tempSubjects[w.tempIndex].getQuestion());
+                w.gs.setNextQuestion(w.tempQuestions[w.questionCounter++]);
             }
         }
 
--- src/quizkampen/SessionHandler.java
+++ src/quizkampen/SessionHandler.java
@@ -1,52 +1,100 @@
 package quizkampen;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SessionHandler {
 
     private SessionQ sessionQ;
-    private States state;
-    private int totalRonds;
-    private int totalQuestionsinRond;
-    private PropertiesReader p;
+    protected Window w;
 
-    private enum States {
-        CONNECTED, WAITINGFOROPPONENTTOCONNECT,
-        PLAYINGGAME, WAITINGFORSERVER, IDLE
+    public SessionHandler(Window w) {
+        this.w = w;
     }
 
-    public SessionHandler(SessionQ sessionQ) {
-        this.sessionQ = sessionQ;
-        p = new PropertiesReader();
-        this.totalRonds = p.getRonds();
-        this.totalQuestionsinRond = p.getQuestionsInRond();
-        this.sessionQ.setTotalRounds(totalRonds);
-        this.sessionQ.setTotalQsInRond(totalQuestionsinRond);
-    }
+    public void checkGame(SessionQ session) {
+        this.sessionQ = session;
+        try {
+            switch (session.getState()) {
+                case WAITING4P1USERNAME: // Servern skapas. UserOne skriver in UserName
+                    System.out.println("waiting4p1userName");
+                    session.setUserNameOne(w.user);
+                    w.setPlayerNumber(1);
+                    w.rs.setResultScreen(session.getTotalQsInRound(), session.getTotalRounds(), w.getUser().getUserName(), "David");
+                    w.rs.setPanel();
+                    w.rs.setActionListener(w.ah);
 
-    public void setState(States state) {
-        this.state = state;
-    }
+                    w.ls.subjectButtons[0].setText(w.session.getSubject().getName());
+                    w.ls.subjectButtons[1].setText(w.session.getSubject().getName());
+                    w.ls.subjectButtons[2].setText(w.session.getSubject().getName());
 
-    public void checkState() {
-        switch (this.state) {
-            case CONNECTED:
+                    w.tempQuestions = new Question[w.session.getTotalQsInRound()];
+                    for (int i = 0; i < 3; i++) {
+                        w.tempSubjects[i] = w.session.getSubject();
+                    }
+                    w.ls.setSubjectButtons(w.tempSubjects);
+//                    w.ls2.opponentLabel.setText(w.session.getUserNameOne().getUserName());
 
-                break;
+                    w.session.setState(State.WAITING4P2USERNAME);
 
-            case WAITINGFOROPPONENTTOCONNECT:
+                    w.add(w.ls);
 
-                break;
+                    w.outGameServer.writeObject(session); // skickar session -> server från P1
+                    w.revalidate();
+                    w.repaint();
+                    break;
 
-            case PLAYINGGAME:
+                case WAITING4P2USERNAME: // UserTwo skriver in UserName
+                    System.out.println("waiting4p2username");
+                    session.setUserNameTwo(w.user);
+                    w.setPlayerNumber(2);
+                    w.add(w.ls2);
+                    w.session.setState(State.WAITINGFORCHOICE);
+                    w.outGameServer.writeObject(session);   // skickar session -> server från P2
+                    w.revalidate();
+                    w.repaint();
+                    break;
 
-                break;
+                case WAITINGFORCHOICE:  // P1 får P2 namn. väljer ämne och spelar
+                    System.out.println("Waiting4Choice - should be only P1");
+                    w.ls2.opponentLabel.setText(w.session.getUserNameTwo().getUserName());
+                    // skickar tillbaka session i ActionHandler när P1 spelat
+                    w.session.setState(State.CHOICESENT);
+                    w.revalidate();
+                    w.repaint();
+                    break;
 
-            case WAITINGFORSERVER:
+                case CHOICESENT:
+                    System.out.println("Choice sent");
+                    w.ls2.subjectButton.setText("Valt ämne?");
+                    w.ls2.setActionListener(w.ah);
+                    w.ls2.readyIconPanel.add(w.ls2.readyButton);
+                    w.ls2.subjectIconPanel.add(w.ls2.subjectButton);
 
-                break;
+                    w.session.setState(State.PLAYGAME);
+                    w.ls2.revalidate();
+                    w.ls2.repaint();
+                    break;
 
-            case IDLE:
+                case PLAYGAME://Chosing Subject, Talar om vems tur de är- väljer ämne.UserTwo får info om ämne.
+                    if (session.getUserChosing() == w.user) {
+                        session.ChangeUserChosing();
+                    }
+                    // Set resultat
+                    // skriver ut resultat till andra spelaren
+                    w.outGameServer.writeObject(w.session);
 
-                break;
+                    break;
+
+                case GAMEOVER:
+                    break;
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Window.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 }
